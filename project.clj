9ad36744a8a3308d95adbb2dfefc42813db60708
Nikolay <artamonov.nikolay@e-queo.com>
2018-02-14 11:09:39 +0300

    update

--- project.clj
+++ project.clj
@@ -5,9 +5,12 @@
             :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [ring "1.4.0"]
-                 [compojure "1.3.4"]]
+                 [compojure "1.3.4"]
+                 [environ "1.0.0"]]
   :main todo-list.core
   :min-lein-version "2.0.0"
-  :uberjar-name "todo-list.jar"
+  :plugins [[environ/environ.lein "0.3.1"]]
+  :hooks [environ.leiningen.hooks]
+  :uberjar-name "todo-list-standalone.jar"
   :profiles {:dev
                 {:main todo-list.core/-dev-main}})
